3ea3d8893d73e43f87d9d63abe2a85b316896336
correctunicycle <54482145+correctunicycle@users.noreply.github.com>
2022-04-14 23:53:25 +0100

    Switched local port to 4000 to facilitate easy comparison with main

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const parsing = require('./public/js/newParser')
 
 const app = express()
 
-const port = 3000
+const port = 4000
 
 app.use(express.static('public'))
 app.use('/css',express.static(__dirname +'public/css'))
